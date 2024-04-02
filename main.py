ef29178742737317306171a5453b3333093db0f8
giovanna <161479843+giovannaarrud@users.noreply.github.com>
2024-04-02 11:33:18 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+print("2-fer")
+
+def Grande():
+    nome = iput("me informe seu nome")
+
+    if not nome:
+        nome = ("voce")
+    else:
+        pass
+
+    print(f"um biscoito para {nome},um biscoito para mim")
+
+    Grande()
